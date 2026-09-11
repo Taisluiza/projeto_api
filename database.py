1bfa89725db540be19e29ae283ef91ab457bb3c6
TaisLuiza <taissalissousa@gmail.com>
2026-09-10 22:03:25 -0300

    Realizando correções para aplicação roda em localhost

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost/escola"
 engine = create_engine(DATABASE_URL)
--- models.py
+++ models.py
@@ -1,31 +1,24 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 class Estudante(Base):    
     __tablename__ = 'estudantes'
-    id = Column(
-        Integer,
-        primary_key=True,
-        index=True
-    )
-    nome = Column(
-        String(100),
-        nullable=False
-    )
+    
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(100), nullable=False)
     age = Column(Integer)
+
+    # relação com Matricula
+    matriculas = relationship("Matricula", back_populates="estudante")
+
 
 class Matricula(Base):
     __tablename__ = 'matriculas'
-    id = Column(
-        Integer,
-        primary_key=True,
-        index=True
-    )
-    student_id = Column(
-        Integer,
-        ForeignKey('estudante.id') #chave estrangeira
-    )
-    nome_disciplina = Column(
-        String(100),
-        nullable=False
-    )
+    
+    id = Column(Integer, primary_key=True, index=True)
+    student_id = Column(Integer, ForeignKey('estudantes.id'))  # chave estrangeira correta
+    nome_disciplina = Column(String(100), nullable=False)
+
+    # relação com Estudante
+    estudante = relationship("Estudante", back_populates="matriculas")
