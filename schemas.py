5deb4af40ed87926bba41f150ddd92a93990111c
TaisLuiza <taissalissousa@gmail.com>
2026-09-10 22:43:01 -0300

    Alterando o arquivo schemas.py, criando a class perfil

--- schemas.py
+++ schemas.py
@@ -1,12 +1,29 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 class EstudanteBase(BaseModel):   
-     
+     id: int
      nome: str
-     idade: int
+     perfil: Optional[Perfil] = None
+
+     class Config:
+      from_attributes = True
 
 class EstudanteCreate(EstudanteBase):
-    pass
+        nome: str
+        email: str
+        perfil: PerfilCreate
+
+class Perfil(BaseModel):
+    id: int
+    idade: int
+    endereco: str
+    class Config:
+        from_attributes = True
+
+class PerfilCreate(BaseModel):
+    idade: int
+    endereco: str
 
 class EstudanteResponse(EstudanteBase):
     id: int
